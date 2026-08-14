878ee8703430d55c758d35b1300fe148d25f7592
wazatoki <wazatoki@gmail.com>
2026-08-14 15:07:22 +0900

    changed container name

--- tools/windows/attach_on_running_container.bat
+++ tools/windows/attach_on_running_container.bat
@@ -1,4 +1,4 @@
 rem attach on running container
 
 cd /d %~dp0
-docker compose -f ..\docker\docker-compose.yml --env-file ..\..\.env exec --user dev my_ssh_container /bin/bash
+docker compose -f ..\docker\docker-compose.yml --env-file ..\..\.env exec --user dev rust_1.97.1_ssh_container /bin/bash
